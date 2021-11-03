c9a470c573ae6d53e9f776282a88e30cc028ee0c
Fallenefc <arylmoraesn@gmail.com>
2021-11-03 17:32:27 -0600

    deployment

--- src/client/pages/api/apiConfig.ts
+++ src/client/pages/api/apiConfig.ts
@@ -4,7 +4,7 @@ const api = () => {
   const accesstoken = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:4000',
+    baseURL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://137.184.158.78:4000',
     headers: {
       Authorization: accesstoken ? accesstoken : '',
     },
